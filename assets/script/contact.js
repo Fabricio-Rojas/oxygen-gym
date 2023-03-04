d52cc0c166e952b459175dfd9016b2f9a45fd1e6
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-03-03 20:26:24 -0600

    Added email inp validation

--- assets/script/contact.js
+++ assets/script/contact.js
@@ -5,6 +5,7 @@
 const contactName = document.querySelector('#contact-name');
 const contactTel = document.querySelector('#contact-tel');
 const contactEmail = document.querySelector('#contact-email');
+const sendBtn = document.querySelector('#send');
 
 // ---------------------- Validate Functions ----------------------
 
@@ -23,5 +24,16 @@ contactTel.addEventListener('keyup', function() {
   
     if(validTel.test(this.value)) {
       this.value = this.value.replace(validTel, "");
+    };
+});
+
+sendBtn.addEventListener('click', function(event) {
+    const validEmail = /\.com$/;
+
+    if (validEmail.test(contactEmail.value)) {
+        return true;
+    } else {
+        alert("Please add a valid email ending in \".com\"");
+        event.preventDefault();
     };
 });
