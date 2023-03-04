78dc22a23c33ccf31029225e6af59bc3bd02315f
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-03-03 18:41:04 -0600

    Added script links and name inp validation

--- assets/script/contact.js
+++ assets/script/contact.js
@@ -2,6 +2,18 @@
 
 // ---------------------- Declaring Document Elements ----------------------
 
+const contactName = document.querySelector('#contact-name');
+const contactTel = document.querySelector('#contact-tel');
+const contactEmail = document.querySelector('#contact-email');
 
+// ---------------------- Validate Functions ----------------------
 
-// ---------------------- Calculator Functions ----------------------
+// const validName = /[a-zA-Z]/;
+
+contactName.addEventListener('keyup', function() {
+    const validName = /[^a-zA-Z\s]/g;
+  
+    if(validName.test(this.value)) {
+      this.value = this.value.replace(validName, "");
+    };
+});
